50d13005a0edcb934e10d7d21cf15401c1116cf4
saurabhrajani <saurabhrajani@gmail.com>
2016-07-09 22:23:03 +0530

    website

--- relocation.php
+++ relocation.php
@@ -1,23 +1,43 @@
 <?php
-	function connectDatabse(){
-		//server name
-		$servername="localhost";
-		//Database Name
-		$dbname = "myDB";
-		//mysql username
-		$username = "root";
-		//mysql password
-		$password = "password";
+	if(isset($_SESSION['role'])){
+		function connectDatabse(){
+			//server name
+			$servername="localhost";
+			//Database Name
+			$dbname = "CFG";
+			//mysql username
+			$username = "root";
+			//mysql password
+			$password = "saurabh@3295";
 
-		$conn = mysql_connect($servername, $username, $password, $dbname);
-		if(!$conn){
-			echo "Error Connection!!";
-			return false;
+			$conn = mysqli_connect($servername, $username, $password, $dbname);
+			if(!$conn){
+				echo "Error Connection!!";
+				return false;
+			}
+			return $conn;
 		}
-		return true;
-	}
-	function relocate(){
 		
-	}
+		$conn = connectDatabse();
 
+		if($conn){
+			$mobile = $_GET['mobile'];
+			$pincode = $_GET['pincode'];
+			$state = $_GET['state'];
+			$city = $_GET['city'];
+			$schoolId = $_GET['schoolId'];
+			$query = "UPDATE TABLE Student_Contact_Details SET Mobile = $mobile, Pin_Code = $pincode, State = $state, $City = $city";
+			echo $query;
+			//mysqli_query($conn, $query) or die("Could not execute query1");
+			$query = "UPDATE TABLE Student School_ID = $schoolId";
+			echo $query;
+			//mysqli_query($conn, $query) or die("Could not execute query2");
+			$query = "UPDATE TABLE Location_History End_Date = $date = date('Y-m-d H:i:s') WHERE End_Date GT = '30-12-9999'";
+			echo $query;
+			//mysqli_query($conn, $query);
+		}
+	}
+	else{
+		header("Location: home.html");
+	}
 ?>
