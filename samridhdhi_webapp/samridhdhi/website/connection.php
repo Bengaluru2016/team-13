063b5905a9f8f266eb2359730f539749f8d94c17
Ranjit <tssranjit@gmail.com>
2016-07-09 18:49:58 +0530

    Database Connection Script
    Database Connection (MySql) Script

---
+++ samridhdhi_webapp/samridhdhi/website/connection.php
@@ -0,0 +1,11 @@
+<!-- This script is used to Connect to Database-->
+<?php
+	//server name
+	$servername="localhost";
+	//Database Name
+	$dbname = "myDB";
+	//mysql username
+	$username = "root";
+	//mysql password
+	$password = "password";
+?>
